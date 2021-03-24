fec7d2a8906bd4051add7b744b102c91e8fd39c9
Jim Wei <jimwei47@live.ca>
2021-03-24 13:28:25 -0400

    web template

---
+++ js/scripts.js
@@ -0,0 +1,155 @@
+/* Template: Gemdev Product Website Bootstrap HTML Template
+   Author: Inovatik
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+    
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    // hover in desktop mode
+    function toggleDropdown (e) {
+        const _d = $(e.target).closest('.dropdown'),
+            _m = $('.dropdown-menu', _d);
+        setTimeout(function(){
+            const shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _m.toggleClass('show', shouldOpen);
+            _d.toggleClass('show', shouldOpen);
+            $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+        }, e.type === 'mouseleave' ? 300 : 0);
+    }
+    $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+    /* Header Waves Animation */
+    const WAVES = document.querySelectorAll('.wave');
+    const CONFIG = [{
+        speed: 40,
+        opacity: 0.3,
+        width: 800 
+    },
+    {
+        speed: 55,
+        opacity: 0.6,
+        width: 800 
+    },
+    {
+        speed: 25,
+        opacity: 1,
+        width: 400 
+    }];
+
+
+    const updateWave = index => () => {
+        for (const key of Object.keys(CONFIG[index])) {
+            WAVES[index].style.setProperty(`--${key}`, CONFIG[index][key]);
+        }
+    };
+
+    WAVES.forEach((_, index) => updateWave(index)());
+
+
+    /* Details Lightbox - Magnific Popup */
+    $('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+    });
+    
+
+    /* Counter - CountTo */
+	var a = 0;
+	$(window).scroll(function() {
+		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
+			var oTop = $('#counter').offset().top - window.innerHeight;
+			if (a == 0 && $(window).scrollTop() > oTop) {
+			$('.counter-value').each(function() {
+				var $this = $(this),
+				countTo = $this.attr('data-count');
+				$({
+				countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+				{
+					duration: 2000,
+					easing: 'swing',
+					step: function() {
+					    $this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+					    $this.text(this.countNum);
+					    //alert('finished');
+					}
+				});
+			});
+			a = 1;
+			}
+		}
+    });
+
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
